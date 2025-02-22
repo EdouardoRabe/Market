42e821c71b21a5b9fddbed18421a7627a7f3e3a4
EdouardoRabe <edouardorabendrazana@gmail.com>
2025-02-22 09:29:02 +0300

    feat: add periode.py and function getPeriode(yearmonth)

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from tkinter import Tk
 from app import MarketApp
 from material import Market
+from tool import Periode
 
 
 if __name__ == "__main__":
-    root = Tk()
-    app = MarketApp(root)
-    root.mainloop()
+    p=Periode.getPeriode("2020-09")
+    print(p.get_idperiode())
+    # root = Tk()
+    # app = MarketApp(root)
+    # root.mainloop()
---
+++ tool/periode.py
@@ -0,0 +1,39 @@
+from connexion import ConnexionAccess
+
+class Periode:
+    def __init__(self, idperiode, debut, fin):
+        self.__idperiode = idperiode
+        self.__debut = debut
+        self.__fin = fin
+
+    def get_idperiode(self):
+        return self.__idperiode
+
+    def get_debut(self):
+        return self.__debut
+
+    def get_fin(self):
+        return self.__fin
+
+    def set_idperiode(self, idperiode):
+        self.__idperiode = idperiode
+
+    def set_debut(self, debut):
+        self.__debut = debut
+
+    def set_fin(self, fin):
+        self.__fin = fin
+
+    @staticmethod
+    def getPeriode(yearMonth):
+        month = int(yearMonth.split('-')[1])
+        conn = ConnexionAccess.getConnexion()
+        query = "SELECT idperiode, debut, fin FROM periodes WHERE ? BETWEEN debut AND fin"
+        cursor = conn.cursor()
+        cursor.execute(query, (month,))
+        row = cursor.fetchone()
+        conn.close()
+        if row:
+            return Periode(row[0], row[1], row[2])
+        else:
+            return None
