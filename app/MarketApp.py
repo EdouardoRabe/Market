2e93390ab0483a5d8c18fb3c6a5e6b032ccdd19c
EdouardoRabe <edouardorabendrazana@gmail.com>
2025-02-25 19:29:08 +0300

    feat: modify insertPaiement add modify css

--- app/MarketApp.py
+++ app/MarketApp.py
@@ -11,46 +11,32 @@ class MarketApp:
         self.root = root
         self.root.title("Market Management App")
         self.root.geometry("800x700")
-
-        # Style personnalisé pour les widgets
         style = ttk.Style()
-
-        # Configuration générale des labels
-        style.configure("TLabel", background="#F0F0F0", foreground="#333333", font=("Arial", 12))
-
-        # Configuration des boutons
-        style.configure("TButton", background="#4CAF50", foreground="#000000", font=("Arial", 12), padding=(10, 5), borderwidth=0, relief="flat", bordercolor="#4CAF50")
-        style.map("TButton", background=[("active", "#388E3C")], foreground=[("active", "#FFFFFF")])
-
-        # Configuration des entrées de texte
+        style.configure("TLabel", background="#F0F8FF", foreground="#333333", font=("Arial", 12))
+        style.configure("TButton", background="#5A9", foreground="#000000", font=("Arial", 12), padding=(8, 4), borderwidth=0, relief="flat")
+        style.map("TButton", background=[("active", "#4A8")], foreground=[("active", "#000000")])
         style.configure("TEntry", fieldbackground="#FFFFFF", foreground="#333333", font=("Arial", 12), padding=5, borderwidth=2, relief="solid", bordercolor="#CCCCCC")
-
-        self.root.configure(bg="#F0F0F0")
-
-        self.yearmonth = tk.StringVar(value="2023-01")
+        self.root.configure(bg="#F0F8FF")
+        self.yearmonth = tk.StringVar(value="2025-01")
         self.yearmonth.trace("w", self.update_title)
-
         self.title_label = ttk.Label(root, text="Situation de Marché au mois de", font=("Arial", 16, "bold"))
-        self.title_label.pack(pady=10)
-
-        self.date_frame = tk.Frame(root, bg="#F0F0F0")
+        self.title_label.pack(pady=5)
+        self.date_frame = tk.Frame(root, bg="#F0F8FF")
         self.date_frame.pack(pady=5)
-
         self.date_label = ttk.Label(self.date_frame, text="Date:")
         self.date_label.pack(side=tk.LEFT, padx=5)
-
         self.date_entry = ttk.Entry(self.date_frame, textvariable=self.yearmonth, width=10, style="TEntry")
         self.date_entry.pack(side=tk.LEFT, padx=5)
-
         self.validate_button = ttk.Button(root, text="Valider", command=self.validate_date, style="TButton")
         self.validate_button.pack(pady=5)
-
         self.payment_button = ttk.Button(root, text="Faire un paiement", command=self.open_web_app, style="TButton")
         self.payment_button.pack(pady=5)
-
-        self.canvas = tk.Canvas(root, width=800, height=600, bg="#FFFFFF")
+        self.top_frame = tk.Frame(root, bg="#F0F8FF")
+        self.top_frame.pack(fill=tk.BOTH, expand=True)
+        self.bottom_frame = tk.Frame(root, bg="#F0F8FF")
+        self.bottom_frame.pack(fill=tk.BOTH, expand=True)
+        self.canvas = tk.Canvas(self.bottom_frame, width=800, height=600, bg="#F0F8FF")
         self.canvas.pack()
-
         self.markets = Market.getMarkets()
         self.update_title()
         self.displayMarkets()
@@ -70,8 +56,8 @@ class MarketApp:
     def displayMarket(self, market):
         x, y = market.get_x(), market.get_y()
         long, larg = market.get_long(), market.get_larg()
-        self.canvas.create_rectangle(x, y, x + long, y + larg, outline="blue", width=2)
-        self.canvas.create_text(x + long / 2, y - 10, text=f"Market ID: {market.get_idmarket()}", fill="blue")
+        self.canvas.create_rectangle(x, y, x + long, y + larg, outline="#5A9", width=2)
+        self.canvas.create_text(x + long / 2, y - 10, text=f"Market ID: {market.get_idmarket()}", fill="#5A9", font=("Helvetica", 10, "bold"))
         boxes = market.getBoxs()
         for box in boxes:
             self.displayBox(box)
@@ -83,11 +69,14 @@ class MarketApp:
         percent_paid = box.getPourcent(yearmonth)
         green_width = int(long * percent_paid)
         red_width = long - green_width
-        if green_width > 0:
-            self.canvas.create_rectangle(x, y, x + green_width, y + larg, outline="green", fill="green", width=2)
-        if red_width > 0:
-            self.canvas.create_rectangle(x + green_width, y, x + long, y + larg, outline="red", fill="red", width=2)
-        self.canvas.create_text(x + long / 2, y + larg / 2, text=f"Box ID: {box.get_idbox()}", fill="black")
+        if(box.get__idowner()==None):
+            self.canvas.create_rectangle(x, y, x + long, y + larg, outline="#DAA520", fill="#DAA520", width=2)
+        else:
+            if green_width > 0:
+                self.canvas.create_rectangle(x, y, x + green_width, y + larg, outline="#4A8", fill="#4A8", width=2)
+            if red_width > 0:
+                self.canvas.create_rectangle(x + green_width, y, x + long, y + larg, outline="#D9534F", fill="#D9534F", width=2)
+        self.canvas.create_text(x + long / 2, y + larg / 2, text=f"Box ID: {box.get_idbox()}", fill="black", font=("Helvetica", 8, "bold"))
 
     def open_web_app(self):
         def start_flask_app():
